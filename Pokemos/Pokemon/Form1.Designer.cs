7a44ae43fd31bb071a821644eb68b6b37803f9d8
Hfroiman <hernan.froiman@alumnos.frgp.utn.edu.ar>
2023-04-26 21:28:43 -0300

    Primercommi. Se creo clase pokemon, PokemoNegocio para la coneccion a DB

---
+++ Pokemos/Pokemon/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Pokemon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwPokemons = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPokemons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwPokemons
+            // 
+            this.dgwPokemons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwPokemons.Location = new System.Drawing.Point(33, 131);
+            this.dgwPokemons.Name = "dgwPokemons";
+            this.dgwPokemons.Size = new System.Drawing.Size(544, 209);
+            this.dgwPokemons.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgwPokemons);
+            this.Name = "Form1";
+            this.Text = "Pestaña de Pokemons";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPokemons)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwPokemons;
+    }
+}
+
---
+++ Pokemos/Pokemon/PokemonNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Pokemon
+{
+    class PokemonNegocio
+    {
+        public List<Pokemon> Listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "Server=.\\SQLEXPRESS; database= POKEDEX_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT Numero, Nombre, Descripcion from POKEMONS";
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    Pokemon obj = new Pokemon();
+
+                    obj.Numero = (int)lector["Numero"];
+                    obj.Nombre = (string)lector["Nombre"];
+                    obj.Descripcion = (string)lector["Descripcion"];
+
+
+                    lista.Add(obj);
+                }
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
